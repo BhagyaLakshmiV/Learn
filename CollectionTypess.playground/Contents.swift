6876fc7d606bae579283486d750098f3c6220af7
Bhagya Lakshmi V <bhagyalv1996@gmail.com>
2022-04-17 23:52:43 +0530

    Collectiontypes

---
+++ CollectionTypess.playground/Contents.swift
@@ -0,0 +1,91 @@
+var someInts: [Int] = []
+print("someInts is of type [Int] with \(someInts.count) items.")
+
+someInts.append(3)
+
+var shoppingList: [String] = ["Eggs", "Milk"]
+print("The shopping list contains \(shoppingList.count) items.")
+
+if shoppingList.isEmpty {
+    print("The shopping list is empty.")
+} else {
+    print("The shopping list isn't empty.")
+}
+
+shoppingList += ["Baking Powder"]
+shoppingList += ["Chocolate Spread", "Cheese", "Butter"]
+
+var firstItem = shoppingList[0]
+shoppingList[0] = "Six eggs"
+
+shoppingList[4...6] = ["Bananas", "Apples"]
+shoppingList.insert("Maple Syrup", at: 0)
+
+let mapleSyrup = shoppingList.remove(at: 0)
+firstItem = shoppingList[0]
+
+let apples = shoppingList.removeLast()
+
+for item in shoppingList {
+    print(item)
+}
+
+for (index, value) in shoppingList.enumerated() {
+    print("Item \(index + 1): \(value)")
+}
+
+var letters = Set<Character>()
+print("letters is of type Set<Character> with \(letters.count) items.")
+
+letters.insert("a")
+
+var favoriteGenres: Set<String> = ["Rock", "Classical", "Hip hop"]
+
+print("I have \(favoriteGenres.count) favorite music genres.")
+
+if favoriteGenres.isEmpty {
+    print("As far as music goes, I'm not picky.")
+} else {
+    print("I have particular music preferences.")
+}
+
+favoriteGenres.insert("Jazz")
+
+if let removedGenre = favoriteGenres.remove("Rock") {
+    print("\(removedGenre)? I'm over it.")
+} else {
+    print("I never much cared for that.")
+}
+
+if favoriteGenres.contains("Funk") {
+    print("I get up on the good foot.")
+} else {
+    print("It's too funky in here.")
+}
+
+for genre in favoriteGenres {
+    print("\(genre)")
+}
+
+let oddDigits: Set = [1, 3, 5, 7, 9]
+let evenDigits: Set = [0, 2, 4, 6, 8]
+let singleDigitPrimeNumbers: Set = [2, 3, 5, 7]
+
+oddDigits.union(evenDigits).sorted()
+oddDigits.intersection(evenDigits).sorted()
+oddDigits.subtracting(singleDigitPrimeNumbers).sorted()
+oddDigits.symmetricDifference(singleDigitPrimeNumbers).sorted()
+
+var namesOfIntegers: [Int: String] = [:]
+
+namesOfIntegers[16] = "sixteen"
+
+var airports: [String: String] = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
+
+print("The airports dictionary contains \(airports.count) items.")
+
+if airports.isEmpty {
+    print("The airports dictionary is empty.")
+} else {
+    print("The airports dictionary isn't empty.")
+}
