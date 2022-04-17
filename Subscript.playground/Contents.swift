da2e8caa202dd92e1bec5f2ca94ea99df065669e
Bhagya Lakshmi V <bhagyalv1996@gmail.com>
2022-04-18 00:01:23 +0530

    Subscript in swift

---
+++ Subscript.playground/Contents.swift
@@ -0,0 +1,81 @@
+struct TimesTable {
+    let multiplier: Int
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
+}
+let threeTimesTable = TimesTable(multiplier: 3)
+print("six times three is \(threeTimesTable[6])")
+
+var numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
+numberOfLegs["bird"] = 2
+
+struct Matrix {
+    let rows: Int, columns: Int
+    var grid: [Double]
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(repeating: 0.0, count: rows * columns)
+    }
+    func indexIsValid(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            grid[(row * columns) + column] = newValue
+        }
+    }
+}
+
+var matrix = Matrix(rows: 2, columns: 2)
+
+matrix[0, 1] = 1.5
+matrix[1, 0] = 3.2
+
+enum Planet: Int {
+    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
+    static subscript(n: Int) -> Planet {
+        return Planet(rawValue: n)!
+    }
+}
+let mars = Planet[4]
+print(mars)
+
+struct subexample {
+   let decrementer: Int
+   subscript(index: Int) -> Int {
+      return decrementer / index
+   }
+}
+let division = subexample(decrementer: 100)
+
+print("The number is divisible by \(division[9]) times")
+print("The number is divisible by \(division[2]) times")
+print("The number is divisible by \(division[3]) times")
+print("The number is divisible by \(division[5]) times")
+print("The number is divisible by \(division[7]) times")
+
+class daysofaweek {
+   private var days = ["Sunday", "Monday", "Tuesday", "Wednesday",
+      "Thursday", "Friday", "saturday"]
+   subscript(index: Int) -> String {
+      get {
+         return days[index]
+      }
+      set(newValue) {
+         self.days[index] = newValue
+      }
+   }
+}
+var p = daysofaweek()
+
+print(p[0])
+print(p[1])
+print(p[2])
+print(p[3])
